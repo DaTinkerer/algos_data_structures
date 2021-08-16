315f0ac260c0d3f71329081ea1dc9e14c778614e
n00bkoder <bryantpeterson5@gmail.com>
2021-08-16 08:20:19 -0400

    learned about hash tables

--- hash_tables.py
+++ hash_tables.py
@@ -1,16 +1,21 @@
 
 class HashTable:
+    # sets an initial array and the size of the array
     def __init__(self):
-        self.MAX = 5
+        self.MAX = 10
         self.arr = [None for i in range(self.MAX)]
     def get_hash(self, key):
         h = 0
         for char in key:
+            #adds the sum of all the ascii codes in each character to h
             h += ord(char)
+        # this gives the key a unique indentifier 
         return h % self.MAX
+    # lets me set any value and give it an index in the array
     def __setitem__(self, key, val):
         h = self.get_hash(key)
         self.arr[h] = val
+    # returns the index of the give key
     def __getitem__(self, key):
         h = self.get_hash(key)
         return self.arr[h]
@@ -23,5 +28,5 @@ t['Mango'] = 9
 t['Peach'] = 5
 t['Pineapple'] = 7
 
-print(t.arr)
+
 
